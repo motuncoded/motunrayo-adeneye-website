36d47a95f26e236f4d0641cd5b6dc1bf2ddb45d7
motuncoded <motunrayoadeneye@example.com>
2023-12-29 18:31:18 +0100

    updated

---
+++ javascript/g.jsx
@@ -0,0 +1,29 @@
+<div class="portfolio-card">
+  <img src="./assests/monify-desktop.png" alt="Monify-landing-page" />
+  <div class>
+    <div class="portfolio-article">
+      <h4>Abdulfatah Portfolio Website</h4>
+      <h5>
+        A portfolio website is an platform that showcasing the skills and
+        experiences of the client as a software developer.
+      </h5>
+    </div>
+    <div class="portfolio-skills">
+      <p>React</p>
+      <p>Vanilla CSS</p>
+      <p>Github Pages</p>
+    </div>
+    <div class="portfolio-icons">
+      <div data-tooltip="github link">
+        <a class="githun-link" href="">
+          <img src="./assests/code.png" width="30px" alt="github-link" />
+        </a>
+      </div>
+      <div data-tooltip="github link">
+        <a href="">
+          <img src="./assests/search-product.png" width="30px" alt="web-link" />
+        </a>
+      </div>
+    </div>
+  </div>
+</div>;
