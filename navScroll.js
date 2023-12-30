e92664894a2115e4cf34ecefe365da36d91aa00e
motuncoded <motunrayoadeneye@example.com>
2023-12-30 13:47:23 +0100

    updated

--- navScroll.js
+++
@@ -1,9 +0,0 @@
-const primaryHeader = document.querySelector(".primary-header");
-const scrollWatch = document.createElement("div");
-
-scrollWatch.setAttribute("data-scroll-watch", "");
-primaryHeader.before(scrollWatch);
-const navObserver = new IntersectionObserver((entries) => {
-  primaryHeader.classList.toggle("sticking");
-});
-navObserver.observe(scrollWatch);
