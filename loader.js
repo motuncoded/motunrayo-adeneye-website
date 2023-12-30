7074cb3670027e6d7ab0d97cb1f313b0554cd157
motuncoded <motunrayoadeneye@example.com>
2023-12-30 10:50:23 +0100

    updated

--- loader.js
+++ loader.js
@@ -1,13 +1,14 @@
-const loader = document.querySelector("#loading");
+const loader = document.getElementById("loading");
 const container = document.querySelector(".container");
 const hero = document.querySelector(".hero");
-const contactHero = document.querySelector(".contact-hero");
+
+
+
 let load = 0;
 setInterval(function () {
   loader.textContent = `${load} %`;
   if (load < 100) {
     load++;
-    contactHero.classList.add("none");
   } else {
     setTimeout(function () {
       container.classList.add("none");
--- main.js
+++ main.js
@@ -3,8 +3,11 @@ import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
+  
+ 
    
   </div>
+
 `;
 
 setupCounter(document.querySelector("#counter"));
